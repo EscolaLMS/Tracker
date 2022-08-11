91e2571fa857f4620014cab1f9624c7fca9a525a
Tomasz Smolarek <tomasz.smolarek@escolasoft.com>
2022-08-11 13:23:13 +0200

    Create track_routes table in provider

--- src/Providers/SqliteServiceProvider.php
+++ src/Providers/SqliteServiceProvider.php
@@ -2,20 +2,29 @@
 
 namespace EscolaLms\Tracker\Providers;
 
+use EscolaLms\Tracker\Database\Schemas\TrackRouteSchema;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class SqliteServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+
+        $connection = config('escolalms_tracker.database.connection');
+
         if (
             config('escolalms_tracker.database.connection') === 'sqlite'
             && config('escolalms_tracker.database.create')
         ) {
-            $databaseFile = __DIR__ . '/../../../../../' . config('escolalms_tracker.database.database');
+            $databaseFile = config('escolalms_tracker.database.path');
 
             if (!file_exists($databaseFile)) {
                 file_put_contents($databaseFile, '');
+            }
+
+            if (!Schema::connection($connection)->hasTable('track_routes')) {
+                Schema::connection($connection)->create('track_routes', TrackRouteSchema::schema());
             }
         }
     }
