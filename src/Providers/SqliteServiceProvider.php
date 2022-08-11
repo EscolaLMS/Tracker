6f76acda7d380398d85388272dff4d524d37fdc6
Tomasz Smolarek <59400506+dyfero@users.noreply.github.com>
2022-08-11 16:25:04 +0200

    add check cache in SqliteServiceProvider (#11)
    Co-authored-by: Tomasz Smolarek <tomasz.smolarek@escolasoft.com>

--- src/Providers/SqliteServiceProvider.php
+++ src/Providers/SqliteServiceProvider.php
@@ -3,6 +3,7 @@
 namespace EscolaLms\Tracker\Providers;
 
 use EscolaLms\Tracker\Database\Schemas\TrackRouteSchema;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -10,23 +11,21 @@ class SqliteServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-
         $connection = config('escolalms_tracker.database.connection');
 
-        if (
-            config('escolalms_tracker.database.connection') === 'sqlite'
-            && config('escolalms_tracker.database.create')
-        ) {
+        if ($connection === 'sqlite' && config('escolalms_tracker.database.create')) {
             $databaseFile = config('escolalms_tracker.database.path');
 
             if (!file_exists($databaseFile)) {
                 file_put_contents($databaseFile, '');
             }
 
-            if (!Schema::connection($connection)->hasTable('track_routes')) {
-                Schema::connection($connection)->create('track_routes', TrackRouteSchema::schema());
+            if (!Cache::get('trackerSqliteCacheCheck')) {
+                if (!Schema::connection($connection)->hasTable('track_routes')) {
+                    Schema::connection($connection)->create('track_routes', TrackRouteSchema::schema());
+                }
+                Cache::put('trackerSqliteCacheCheck', true);
             }
         }
     }
-
 }
